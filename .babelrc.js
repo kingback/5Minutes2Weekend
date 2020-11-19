34153bcca05d499f7074a05823a3fa601a946243
虎牙 <huya.nzb@alibaba-inc.com>
2020-11-19 23:56:53 +0800

    fix(jest): fix babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     ['@babel/preset-env', {
       loose: true,
-      modules: process.env.PRESET_ENV_MODULES || false
+      modules: process.env.PRESET_ENV_MODULES === 'esm' ? false : 'commonjs'
     }]
   ],
   plugins: [
